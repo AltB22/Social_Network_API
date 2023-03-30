b865961edde39d053fb1f8130257cc884997ff1f
Alt B 2022 <massie.w@gmail.com>
2023-03-30 00:00:28 -0700

    Commented out all models but user to test for path dependency. Successful test of npm start command to start server on port 3000.  Re-ran npm init and npm install prior

--- models/friend.js
+++ models/friend.js
@@ -1,24 +1,24 @@
-const { Schema, model } = require('mongoose');
-// const thoughtSchema = require('./thought');
-const userSchema = require('./user');
+// const { Schema, model } = require('mongoose');
+// // const thoughtSchema = require('./thought');
+// const userSchema = require('./user');
 
-const friendSchema = new Schema(
-  {
-    username: {
-      type: String,
-      unique: true,
-      reqired: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      match: [],//Must match a valid email address (look into Mongoose's matching validation),
-    },
-    // thoughts: [thoughtSchema],
-    users: [userSchema],
-  }
-)
-const Friend = model('friend', friendSchema);
-module.exports =  Friend;
+// const friendSchema = new Schema(
+//   {
+//     username: {
+//       type: String,
+//       unique: true,
+//       reqired: true,
+//       trim: true,
+//     },
+//     email: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//       match: [],//Must match a valid email address (look into Mongoose's matching validation),
+//     },
+//     // thoughts: [thoughtSchema],
+//     users: [userSchema],
+//   }
+// )
+// const Friend = model('friend', friendSchema);
+// module.exports =  Friend;
--- models/reaction.js
+++ models/reaction.js
@@ -1,34 +1,34 @@
-const { Schema, model } = require('mongoose');
-const userSchema = require('./user')
+// const { Schema, model } = require('mongoose');
+// const userSchema = require('./user')
 
-const reactionSchema = new Schema(
-  {
-    courseName: {
-      type: String,
-      required: true,
-    },
-    inPerson: {
-      type: Boolean,
-      default: true,
-    },
-    startDate: {
-      type: Date,
-      default: Date.now(),
-    },
-    endDate: {
-      type: Date,
-      default: () => new Date(+new Date() + 84 * 24 * 60 * 60 * 1000),
-    },
-    users: [userSchema],
-  },
-  {
-    toJSON: {
-      virtuals: true,
-    },
-    id: true,
-  }
-);
+// const reactionSchema = new Schema(
+//   {
+//     courseName: {
+//       type: String,
+//       required: true,
+//     },
+//     inPerson: {
+//       type: Boolean,
+//       default: true,
+//     },
+//     startDate: {
+//       type: Date,
+//       default: Date.now(),
+//     },
+//     endDate: {
+//       type: Date,
+//       default: () => new Date(+new Date() + 84 * 24 * 60 * 60 * 1000),
+//     },
+//     users: [userSchema],
+//   },
+//   {
+//     toJSON: {
+//       virtuals: true,
+//     },
+//     id: true,
+//   }
+// );
 
-const Reacton = model('reaction', reactionSchema);
+// const Reacton = model('reaction', reactionSchema);
 
-module.exports = Reacton;
+// module.exports = Reacton;
