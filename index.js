da043874a46e7c39a1d62a61aef0a44928cb507f
Alt B 2022 <massie.w@gmail.com>
2023-03-29 22:56:27 -0700

    Added some notes to models/index for non-relational / noSQL db structure.  Set up all my routes in Insomnia ahead of time (obv no code to push for that but helpful step to prep for building and understanding the routes that are needed.

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const routes = require('./routes');
 
 const cwd = process.cwd();
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.port || 3000;
 const app = express();
 
 // Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
-const activity = cwd.includes('01-Activities')
-  ? cwd.split('/01-Activities/')[1]
-  : cwd;
+// const activity = cwd.includes('01-Activities')
+//   ? cwd.split('/01-Activities/')[1]
+//   : cwd;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,6 +18,6 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server for ${activity} running on port ${PORT}!`);
+    console.log(`API server for running on port ${PORT}!`);
   });
 });
--- models/index.js
+++ models/index.js
@@ -2,3 +2,11 @@ const Student = require('./Student');
 const Course = require('./Course');
 
 module.exports = { Student, Course };
+
+//some notes on mongodb / non - relational databases in general:
+
+//Database
+    //Collection (with no schema enforced)
+        //Document (BSON object - look like typ. JS objects)
+            //Fields as key : value pairs
+            //Related data nested in embedded document
